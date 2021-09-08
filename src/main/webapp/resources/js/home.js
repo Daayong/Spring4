34ddc7df78280655b2da73462f4d2c47ccaddaeb
Daayong <87287107+Daayong@users.noreply.github.com>
2021-09-08 15:31:45 +0900

    sorrycommit

--- src/main/webapp/resources/js/home.js
+++ src/main/webapp/resources/js/home.js
@@ -2,29 +2,28 @@
  * 
  */
  
- var num=10;
- console.log(num);
- num='abc';
-console.log(num); 
-
-let d3=document.getElementById("d3");
-alert(d3.src);
-
-
-
  
-/*let d1=document.getElementById("d1");
+ var num = 10;
+ console.log(num);
+ num = 'abc';
+ console.log(num);
+ 
+/* let c1= document.getElementsByClassName("c1");
+ alert(c1);
+ console.log(c1);*/
+ 
+ let d3 = document.getElementById("d3");
+ alert(d3.src);
+ d3.src="./resources/images/iu3.jpg";
+ alert(d3.class);
+ 
+ 
+/*let d1 = document.getElementById("d1");
 alert(d1.innerHTML);
-console.log(d1.innerHTML);
-d1.innerHTML='<img class="c1" alt="img1" src="./resources/images/sharpei.jpg">'
-*/
-
-/*let d2=document.getElementById("d2");
-alert(d2.innerText);
-console.log(d2.innerText);
-*/
-
-/*let c1=document.getElementsByClassName("c1");
-alert("c1");
-console.log("c1");*/
-
+console.log(d1.innerHTML); 
+d1.innerHTML='<img class="c1" alt="" src="./resources/images/iu2.jpg">'
+ */
+/* let check = window.confirm("OK???");
+ console.log(check);*/
+ 
+ 
