fce291ff6b72aef9500de0d004721de8d5e5b023
Daayong <87287107+Daayong@users.noreply.github.com>
2021-09-29 10:20:11 +0900

    commit

--- src/main/java/com/iu/s4/board/notice/NoticeController.java
+++ src/main/java/com/iu/s4/board/notice/NoticeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -86,6 +87,7 @@ public class NoticeController {
 		
 		return mv;	
 	}
+	
 	
 	@GetMapping("select")
 	public ModelAndView getSelect(BoardDTO boardDTO) throws Exception {
